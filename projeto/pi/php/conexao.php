c8a6635d885697bb7ec58a215e4d9a73d0c2aecc
Matheus Stolf <matheus55771@outlook.com>
2024-03-30 18:17:15 -0300

    v1.1 documentacao

---
+++ projeto/pi/php/conexao.php
@@ -0,0 +1,22 @@
+<?php
+// Função para criar a conexão PDO com o banco de dados
+function conectarBanco() {
+    $host = 'localhost'; // Nome do host
+    $dbname = 'sistema'; // Nome do banco de dados
+    $usuario = 'root'; // Nome de usuário do banco de dados
+    $senha = ''; // Senha do banco de dados
+
+    // Tente se conectar ao banco de dados
+    try {
+        $conexao = new PDO("mysql:host=$host;dbname=$dbname", $usuario, $senha);
+        // Define o modo de erro do PDO como exceção
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return 'sucesso';
+        
+    } catch(PDOException $e) {
+        // Em caso de erro na conexão, exibe o erro
+        echo 'Erro na conexão com o banco de dados: ' . $e->getMessage();
+        exit;
+    }
+}
+?>
